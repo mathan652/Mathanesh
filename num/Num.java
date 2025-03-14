2d5e86b56957c42567ec3929d4692c16e65b29cc
Your Name <mathaneshmeera@gmail.com>
2025-03-14 10:42:20 +0530

    yamiiii

---
+++ num/Num.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Num{
+    public static void main(String[] args) {
+        int i=0;
+        Scanner obj=new Scanner(System.in);
+        System.out.println("enter the number");
+        int n=obj.nextInt();
+        while (i<=n) {
+            System.out.println(i);
+            i++;
+            }
+        } 
+        
+}
