63b07b5022d777eb6e82915ec5756ea06f534294
Your Name <mathaneshmeera@gmail.com>
2025-03-14 14:28:01 +0530

    yzzhsdy

---
+++ statement/state.java
@@ -0,0 +1,26 @@
+public class state {
+    public static void main(String[] args) {
+        child2 obj=new child2();
+        obj.print();
+        child obj1=new child();
+        obj1.display();
+
+    }
+}
+class parent{
+    public void add(){
+        System.out.println("this is parent class");
+    
+    } 
+}
+class child extends parent{
+    void display(){
+        add();
+
+    }
+}
+class child2 extends parent{
+    void print(){
+        add();
+    }
+}
