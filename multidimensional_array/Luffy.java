e58b625b52d7bc0c7f7b111207677956f1a8289c
Your Name <mathaneshmeera@gmail.com>
2025-03-14 10:19:57 +0530

    babiiii

---
+++ multidimensional_array/Luffy.java
@@ -0,0 +1,10 @@
+public class Luffy{
+    public static void main(String[]args){
+        int a[][]={{10,20},{10,20}};
+        System.out.println(a[0][0]); 
+        System.out.println(a[0][1]);
+        System.out.println(a[1][0]);
+        System.out.println(a[1][1]);
+
+    }
+}
