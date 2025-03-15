6732fa6fd2ba950b7b1dd214505390be16b403c6
Your Name <mathaneshmeera@gmail.com>
2025-03-15 11:05:51 +0530

    vtdf

---
+++ constructor/Leo.java
@@ -0,0 +1,14 @@
+
+public class Leo{
+    Leo(){
+        System.out.println("hello world");
+    }
+    
+
+    
+    public static void main(String[] args) {
+      Leo obj=new Leo();
+     
+
+    }
+}
