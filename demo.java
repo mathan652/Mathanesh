7813a93ac6b03af11cc4a1306998b16155870d11
Your Name <mathaneshmeera@gmail.com>
2025-03-15 14:36:06 +0530

    grtfe

---
+++ demo.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class demo {
+    demo(){
+        System.out.println("hello world");
+    }
+    
+
+    
+    public static void main(String[] args) {
+      demo obj=new demo();
+     
+
+    }
+}
